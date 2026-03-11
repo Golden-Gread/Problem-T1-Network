70e2da0e3532e23ec808c0a4881176ace41168d5
Golden-Gread <778966179@qq.com>
2026-03-11 23:08:11 +0800

    Add files via upload

---
+++ problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+map<string,string> mp;
+
+bool cheak(string addr){
+	int a,b,c,d,e;
+	sscanf(addr.c_str(),"%d.%d.%d.%d.%d",&a,&b,&c,&d,&e);
+	
+}
+
+
+void create(){
+    string s,e;cin>>s>>e;
+    
+}
+
+
+
+int main(){
+   
+
+}
